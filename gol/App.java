1c1a826c8d1e315ff4055dcc5f792d71c80de2fc
evanwin123 <ew6740@pleasantonusd.net>
2025-12-13 05:07:57 +0000

    final project commit ; had to reset everything beacuse code wasnt  working

--- gol/App.java
+++ gol/App.java
@@ -2,20 +2,8 @@ package gol;
 
 public class App {
 
-    public static void main(String[] args)
-    {
-        // GameOfLife g = new GameOfLife(7, 7);
-
-        // int[][] data = {
-        //     { 0, 1, 0},
-        //     { 0, 0, 1},
-        //     { 1, 1, 1 }
-        // };
-
-        // g.set(1, 1, data);
-
-        // g.run(5);
-
-        TestSuite.run();
-    }
+   public static void main(String[] args)
+   {
+       TestSuite.run();
+   }
 }
--- gol/TestSuite.java
+++ gol/TestSuite.java
@@ -3,61 +3,42 @@ package gol;
 import java.util.Arrays;
 
 public class TestSuite {
-    public static void run()
-    {
-        System.out.println("Starting TestSuite");
-        boolean pass = true;
+   public static void run()
+   {
+       System.out.println("Starting TestSuite");
+       boolean pass = true;
 
-        GameOfLife board = new GameOfLife(5, 5);
+       GameOfLife board = new GameOfLife(5, 5);
 
-        // Test count neighbors
-        int[][] data = {{ 1 }};
+       int[][] data = {{ 1 }};
 
-        // Set a single cell, then check that it has zero neighbors.
-        board.set(2, 2, data);
-        pass &= expect(board.countNeighbors(2,2), 0, "Single live cell with zero neighbors");
+       board.set(2, 2, data);
+       pass &= expect(board.countNeighbors(2,2), 0, "Single live cell with zero neighbors");
 
-        // Set the middle top neighbor.
-        board.set(1, 2, data);
-        pass &= expect(board.countNeighbors(2,2), 1, "Single live cell with one neighbor");
+       board.set(1, 2, data);
+       pass &= expect(board.countNeighbors(2,2), 1, "Single live cell with one neighbor");
 
-        // Set the bottom middle neighbor.
-        board.set(3, 2, data);
-        pass &= expect(board.countNeighbors(2,2), 2, "Single live cell with two neighbors");
+       board.set(3, 2, data);
+       pass &= expect(board.countNeighbors(2,2), 2, "Single live cell with two neighbors");
 
-        // Test update state
-        board.step();
-        board.print();
-        pass &= expect(board.get(2,3), 1, "Expect line to rotate");
+       board.step();
+       board.print();
+       pass &= expect(board.get(2,3), 1, "Expect line to rotate");
 
-        board.step();
-        board.print();
-        pass &= expect(board.get(1,2), 1, "Expect line to rotate");
+       board.step();
+       board.print();
+       pass &= expect(board.get(1,2), 1, "Expect line to rotate");
 
+       if (pass)
+           System.out.println("--- TEST PASSED! Congrats! ---");
+       else
+           System.out.println("--- TEST FAILED! :( ---");
+   }
 
-        if (pass == true)
-        {
-            System.out.println("--- TEST PASSED! Congrats! ---");
-        }
-        else
-        {
-            System.out.println("--- TEST FAILED! :( ---");
-        }
-    }
-
-    private static boolean expect(int input, int expected, String comment)
-    {
-        if (input == expected)
-        {
-            return true;
-        }
-        System.out.println(comment + ", value: " + input + " not equal to expected: " + expected);
-        return false;
-    }
-
-    private static boolean testPattern(int[][] input, int[][] expected)
-    {
-        return true;
-    }
-
+   private static boolean expect(int input, int expected, String comment)
+   {
+       if (input == expected) return true;
+       System.out.println(comment + ", value: " + input + " not equal to expected: " + expected);
+       return false;
+   }
 }
