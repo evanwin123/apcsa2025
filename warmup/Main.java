49435a8f867612bb4c4280ca459d5b46b7941f81
evanwin123 <ew6740@pleasantonusd.net>
2026-01-09 05:57:43 +0000

    first

--- warmup/Main.java
+++ warmup/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         // Your code here.
-
+    int sum = 0;
+for (int i = 1; i < 1000; =+ i++) {
+ if (i % 3 == 0 || i % 5 == 0)  {
+    sum += i;
+ }
+}
+System.out.println(sum);
     }
 }
+
+
