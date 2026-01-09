1da2ec3c98508cb1b5daca9df73ba29874d658ac
evanwin123 <ew6740@pleasantonusd.net>
2026-01-09 06:36:41 +0000

    fib

--- warmup/Main.java
+++ warmup/Main.java
@@ -11,9 +11,29 @@ for (int i = 1; i < 1000; i++) {
  }
 }
 System.out.println(sum);
+
+    int fib1 = 1;
+int fib2= 2;
+int fibsum = 2;
+
+while (fib2 < 4000000) {
+    int next = fib1 + fib2;
+
+    if(next > 4000000) {
+        break;
     }
+    if (next % 2 == 0) {
+        fibsum += next;
+    }
+    fib1 = fib2;
+    fib2 = next;
+    }
+System.out.println (fibsum);
+}
 }
 
-int 
+
+
+
 
 
