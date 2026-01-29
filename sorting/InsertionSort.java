4f1a55bdf51177fed4c40d117c89084db23d811f
evanwin123 <ew6740@pleasantonusd.net>
2026-01-29 06:59:35 +0000

    insertion done

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -8,3 +8,13 @@ public class InsertionSort implements Sorter {
         for (int i = 1; i < input.length; i++) {
             int current = input[i];
             int j = i - 1;
+
+            while (j >= 0 && input[j] > current) {
+                input[j + 1] = input[j];
+                j = j - 1;
+            }
+            
+            input[j + 1] = current;
+        }
+    }
+}
