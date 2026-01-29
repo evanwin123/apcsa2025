67cdd4f5923afc327c77bc65b5c9b42c7cb3f966
evanwin123 <ew6740@pleasantonusd.net>
2026-01-29 06:58:42 +0000

    second

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,5 +4,7 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
-    }
-}
+        
+        for (int i = 1; i < input.length; i++) {
+            int current = input[i];
+            int j = i - 1;
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,6 +4,7 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+    
     }
 }
 // lol
